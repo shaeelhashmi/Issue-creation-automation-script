8319ad3c24e8bae159725987fde1002974d70674
shaeelhashmi <shaeelh001@gmail.com>
2025-03-18 11:28:49 +0500

    Fixed some issues

--- issues.py
+++ issues.py
@@ -21,9 +21,22 @@ options.add_argument(firefox_profile_path)
 # Set up the Firefox driver with the profile
 service = Service(gecko_path)
 browser = webdriver.Firefox(service=service, options=options)
+browser.get("https://github.com/shaeelhashmi/Issue-testing/issues")
+time.sleep(5)
+already_made_issues=browser.find_elements(By.CLASS_NAME,"IssueRow-module__row--XmR1f")
+# Create a hash set to store already made issues
+already_made_issues_set = set()
 
-# Open the website
-browser.get("https://github.com/shaeelhashmi/Issue-testing/issues/new?template=BLANK_ISSUE")
+# Add all the already made issues to the hash set
+for issue in already_made_issues:
+    text=issue.find_element(By.TAG_NAME,"a").text
+    print(text)
+    already_made_issues_set.add(text)
+
+browser.execute_script(f"window.open('https://github.com/shaeelhashmi/Issue-testing/issues/new?template=BLANK_ISSUE', '_blank');")
+browser.close()
+browser.switch_to.window(browser.window_handles[0])
+time.sleep(5)
 print("Visiting......")
 dictionary={}
 elementCount=0
@@ -42,13 +55,15 @@ while elementCount < 10:
                 EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[5]/div/main/react-app/div/div/div/div/div[7]/div/div[1]/div[1]/div/div/div/div/div[2]/div[1]"))
             ).get_attribute("outerHTML")
             title=browser.find_element(By.XPATH,"/html/body/div[1]/div[5]/div/main/react-app/div/div/div/div/div[1]/div/div/div[1]/h1/bdi").text
+            if title in already_made_issues_set:
+                raise Exception("Already made issue")
             dictionary[title] = description
             
             elementCount+=1
             if elementCount>=10:
                 break
-        except:
-            print("Error")
+        except Exception as e:
+            print(f"Error: {e}")
         finally:
             browser.close()
             browser.switch_to.window(browser.window_handles[-1])
